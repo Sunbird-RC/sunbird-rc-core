85b40d256582c868ed162d242b0918147515c777
jyotsna <jyotsna.raveendran@tarento.com>
2018-01-09 12:30:23 +0530

    Issue #3 - JsonLD to RDF Conversion

---
+++ java/utils/converters/jsonldToRDF/src/main/java/io/opensaber/utils/converters/JavaJsonLdToRdfConverter.java
@@ -0,0 +1,47 @@
+package io.opensaber.utils.convertors;
+
+import java.io.InputStream;
+import java.io.Reader;
+
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
+
+
+public class JavaJsonLdToRdfConverter {
+
+	public static String jsonldToRDFConvertor(InputStream inputStream){
+		String rdfStr = "";
+		try{
+			Object compact = JsonLdProcessor.toRDF(JsonUtils.fromInputStream(inputStream));
+			rdfStr = JsonUtils.toPrettyString(compact);
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
+		return rdfStr;
+	}
+	
+	public static String jsonldToRDFConvertor(Reader reader){
+		String rdfStr = "";
+		try{
+			Object compact = JsonLdProcessor.toRDF(JsonUtils.fromReader(reader));
+			rdfStr = JsonUtils.toPrettyString(compact);
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
+		return rdfStr;
+	}
+	
+	public static String jsonldToRDFConvertor(String jsonString){
+		String rdfStr = "";
+		try{
+			Object compact = JsonLdProcessor.toRDF(JsonUtils.fromString(jsonString));
+			rdfStr = JsonUtils.toPrettyString(compact);
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
+		return rdfStr;
+	}
+}
