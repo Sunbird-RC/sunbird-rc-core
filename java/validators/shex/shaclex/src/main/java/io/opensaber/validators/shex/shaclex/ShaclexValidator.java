3b1b9861ab7d27b75879e1b8019261be7017e293
Shashank Teotia <shashankteotia@gmail.com>
2018-01-11 13:45:17 +0530

    Issue #4 feat: MVP ShEx Validator

--- java/validators/shex/shaclex/src/main/java/io/opensaber/validators/shex/shaclex/ShaclexValidator.java
+++ java/validators/shex/shaclex/src/main/java/io/opensaber/validators/shex/shaclex/ShaclexValidator.java
@@ -1,18 +1,52 @@
 package io.opensaber.validators.shex.shaclex;
 
-import java.io.File;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import es.weso.schema.*;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.rdf.model.Model;
-
-import es.weso.rdf.jena.RDFAsJenaModel;
+import scala.collection.JavaConverters;
 import scala.Option;
-import scala.util.Try;
+
+import es.weso.rdf.RDFReader;
+import es.weso.rdf.jena.RDFAsJenaModel;
 
 public class ShaclexValidator {
 
-	public void validate(File arg0, String arg1, Option<String> arg2){
-		// TODO Auto-generated method stub
-		Try<RDFAsJenaModel> model = RDFAsJenaModel.fromFile(arg0, arg1, arg2);
+	Option<String> none = Option.apply(null);
+
+	public void validate(Model dataModel, Schema schema) throws Exception {
+		RDFReader rdf = new RDFAsJenaModel(dataModel);
+		Result result = schema.validate(rdf,"TARGETDECLS",none,none, rdf.getPrefixMap(), schema.pm());
+		if (result.isValid()) {
+			System.out.println("Result is valid");
+			System.out.println("Valid. Result: " + result.show());
+			List<Solution> solutions = JavaConverters.seqAsJavaList(result.solutions());
+			solutions.forEach((solution) ->
+			System.out.println(solution.show()));
+		} else {
+			System.out.println("Not valid");
+			List<ErrorInfo> errors = JavaConverters.seqAsJavaList(result.errors());
+			errors.forEach((e) ->
+			System.out.println(e.show()));
+		}
+	} 
+
+	public void validate(String dataFile, String schemaFile, String schemaFormat, String processor) throws Exception {
+		System.out.println("Reading data file " + dataFile);
+		Model dataModel = RDFDataMgr.loadModel(dataFile);
+		System.out.println("Model read. Size = " + dataModel.size());
+		System.out.println(dataModel);
+		System.out.println("Reading shapes file " + schemaFile + " with format " + schemaFormat);
+		Schema schema = readSchema(schemaFile,schemaFormat, processor);
+		System.out.println("Schema read" + schema.serialize(schemaFormat));
+		validate(dataModel,schema);
+	}
+
+	public Schema readSchema(String schemaFile, String format, String processor) throws Exception {
+		String contents = new String(Files.readAllBytes(Paths.get(schemaFile)));
+		return Schemas.fromString(contents,format,processor,none).get();
 	}
 
 }
--- java/validators/shex/shaclex/src/test/java/io/opensaber/validators/shex/shaclex/ShaclexValidatorTest.java
+++ java/validators/shex/shaclex/src/test/java/io/opensaber/validators/shex/shaclex/ShaclexValidatorTest.java
@@ -3,10 +3,18 @@ package io.opensaber.validators.shex.shaclex;
 import org.junit.Test;
 
 public class ShaclexValidatorTest {
+	
+	public static final String SCHEMAFORMAT = "SHEXC";
+	public static final String PROCESSOR 	= "shex";
 
 	@Test
-	public void testValidate() throws Exception {
-		throw new RuntimeException("not yet implemented");
+	public void testValidateModelSchema() throws Exception {
+		ShaclexValidator validator = new ShaclexValidator();
+		validator.validate(getPath("good1.ttl"), getPath("good1.shex"), SCHEMAFORMAT, PROCESSOR);
+	}
+
+	private String getPath(String file) {
+		return this.getClass().getResource("/"+file).getPath();
 	}
 
 }
