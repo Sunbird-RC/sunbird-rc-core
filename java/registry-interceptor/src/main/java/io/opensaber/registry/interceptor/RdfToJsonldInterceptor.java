e479c0eabdeaca9b4cb15069bd2fb3e12ef1dcbd
jyotsna <jyotsna.raveendran@tarento.com>
2018-01-23 15:53:06 +0530

    Issue #10 feat: changes in pom file

--- java/registry-interceptor/src/main/java/io/opensaber/registry/interceptor/RdfToJsonldInterceptor.java
+++ java/registry-interceptor/src/main/java/io/opensaber/registry/interceptor/RdfToJsonldInterceptor.java
@@ -8,17 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.github.jsonldjava.core.JsonLdProcessor;
-import com.github.jsonldjava.utils.JsonUtils;
-
 import io.opensaber.registry.interceptor.handler.BaseResponseHandler;
-import io.opensaber.registry.interceptor.handler.ResponseWrapper;
-import io.opensaber.registry.middleware.impl.JsonldToRdfConverter;
 import io.opensaber.registry.middleware.impl.RdfToJsonldConverter;
 import io.opensaber.registry.middleware.util.Constants;
 
