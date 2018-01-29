3512965a6b82b0d0fd44ad177da88050319abb40
Shashank Teotia <shashankteotia@gmail.com>
2018-01-29 10:56:32 +0530

    Issue #13 feat: RDF Validation Middleware

---
+++ java/middleware/registry-middleware/rdf-validation/src/main/java/io/opensaber/registry/middleware/impl/RDFValidation.java
@@ -0,0 +1,61 @@
+package io.opensaber.registry.middleware.impl;
+
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.relation.RelationService;
+
+import org.apache.jena.rdf.model.Model;
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.github.jsonldjava.core.JsonLdConsts;
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
+
+import es.weso.schema.Schema;
+import io.opensaber.registry.middleware.BaseMiddleware;
+import io.opensaber.registry.middleware.MiddlewareHaltException;
+import io.opensaber.registry.middleware.util.Constants;
+import io.opensaber.validators.shex.shaclex.ShaclexValidator;
+
+/**
+ * 
+ * @author steotia
+ *
+ */
+public class RDFValidation implements BaseMiddleware{
+
+	private static final String RDF_DATA_IS_MISSING = "RDF Data is missing!";
+	private Path schemaFilePath;	
+	public static final String SCHEMAFORMAT = "SHEXC";
+	public static final String PROCESSOR 	= "shex";
+	
+	public RDFValidation(Path schemaFilePath){
+		this.schemaFilePath = schemaFilePath;
+	}
+
+	public Map<String, Object> execute(Map<String, Object> mapData) throws IOException, MiddlewareHaltException {
+		Object RDF = mapData.get("RDF");
+		if (RDF==null) {
+			throw new MiddlewareHaltException(this.getClass().getName()+RDF_DATA_IS_MISSING); 
+		} else if (RDF instanceof Model) {
+			ShaclexValidator validator = new ShaclexValidator();
+			Schema schema = validator.readSchema(this.schemaFilePath,SCHEMAFORMAT, PROCESSOR);
+			validator.validate((Model)RDF, schema);
+		} else {
+			throw new MiddlewareHaltException(this.getClass().getName()+"RDF Data is invalid!");
+		}
+		return null;
+	}
+
+	public Map<String, Object> next(Map<String, Object> mapData) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
