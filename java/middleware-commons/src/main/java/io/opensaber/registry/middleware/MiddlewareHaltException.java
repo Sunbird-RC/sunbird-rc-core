d605de6b270d6f9863935377198e163a40d8433b
Shashank Teotia <shashankteotia@gmail.com>
2018-01-29 10:50:52 +0530

    Issue #11 feat: Adding a Base and Halt Exception

---
+++ java/middleware-commons/src/main/java/io/opensaber/registry/middleware/MiddlewareHaltException.java
@@ -0,0 +1,14 @@
+package io.opensaber.registry.middleware;
+
+public class MiddlewareHaltException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4684320522502865642L;
+
+	public MiddlewareHaltException(String message){
+		super(message);
+	}
+
+}
