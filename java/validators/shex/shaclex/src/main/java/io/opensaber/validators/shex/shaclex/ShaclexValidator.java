f80f5bc8421af107e43e4b7b0078e38426aa9fd8
Shashank Teotia <shashankteotia@gmail.com>
2018-01-29 10:53:29 +0530

    Issue #4 feat: ShEx Validator edits

--- java/validators/shex/shaclex/src/main/java/io/opensaber/validators/shex/shaclex/ShaclexValidator.java
+++ java/validators/shex/shaclex/src/main/java/io/opensaber/validators/shex/shaclex/ShaclexValidator.java
@@ -1,7 +1,9 @@
 package io.opensaber.validators.shex.shaclex;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import es.weso.schema.*;
@@ -19,7 +21,7 @@ public class ShaclexValidator {
 
 	Option<String> none = Option.apply(null);
 
-	private void validate(Model dataModel, Schema schema) throws Exception {
+	public void validate(Model dataModel, Schema schema) {
 		RDFReader rdf = new RDFAsJenaModel(dataModel);
 		Result result = schema.validate(rdf,"TARGETDECLS",none,none, rdf.getPrefixMap(), schema.pm());
 		if (result.isValid()) {
@@ -36,13 +38,13 @@ public class ShaclexValidator {
 		}
 	} 
 
-	public void validate(String dataJSONLD, String schemaFile, String schemaFormat, String processor) throws Exception {
+	public void validate(String dataJSONLD, String schemaFile, String schemaFormat, String processor) throws IOException {
 		System.out.println("Reading data JSONLD " + dataJSONLD);
 		Model dataModel = parse(dataJSONLD);//RDFDataMgr.loadModel(dataFile);
 		System.out.println("Model read. Size = " + dataModel.size());
 		System.out.println(dataModel);
 		System.out.println("Reading shapes file " + schemaFile + " with format " + schemaFormat);
-		Schema schema = readSchema(schemaFile,schemaFormat, processor);
+		Schema schema = readSchema(Paths.get(schemaFile),schemaFormat, processor);
 		System.out.println("Schema read" + schema.serialize(schemaFormat));
 		validate(dataModel,schema);
 	}    
@@ -54,8 +56,8 @@ public class ShaclexValidator {
         return m;
 	}
 
-	private Schema readSchema(String schemaFile, String format, String processor) throws Exception {
-		String contents = new String(Files.readAllBytes(Paths.get(schemaFile)));
+	public Schema readSchema(Path schemaFilePath, String format, String processor) throws IOException {
+		String contents = new String(Files.readAllBytes(schemaFilePath));
 		return Schemas.fromString(contents,format,processor,none).get();
 	}
 
