d9321c9f07907a0580d23f35e296216ca1324070
Shashank Teotia <shashankteotia@gmail.com>
2018-01-30 15:11:55 +0530

    Issue #13 feat: RDF Validation Middleware

--- java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/Constants.java
+++ java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/Constants.java
@@ -10,5 +10,7 @@ public class Constants {
 	public static final String REQUEST_ATTRIBUTE_NAME = "dataObject";
 	public static final String RESPONSE_ATTRIBUTE_NAME = "dataObject";
 	public static final String ATTRIBUTE_NAME = "dataObject";
+	public static final String RDF_OBJECT = "RDF";
+	public static final String RDF_VALIDATION_OBJECT = "RDFValidationResult";
 
 }
