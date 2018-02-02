d9f1914d9dcfa8902c3f924218ad96b33db2e5d8
jyotsna <jyotsna.raveendran@tarento.com>
2018-02-02 18:09:45 +0530

    Issue #15: feat: changes in create API and tests

--- java/registry/src/test/java/io/opensaber/registry/dao/impl/RegistryDaoImplTest.java
+++ java/registry/src/test/java/io/opensaber/registry/dao/impl/RegistryDaoImplTest.java
@@ -27,15 +27,29 @@ import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.io.IoCore;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 
 import io.opensaber.registry.config.CassandraConfiguration;
 import io.opensaber.registry.dao.RegistryDao;
 import io.opensaber.registry.model.dto.EntityDto;
 import io.opensaber.registry.util.GraphDBFactory;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.UUID;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
@@ -55,6 +69,9 @@ public class RegistryDaoImplTest {
 	private static TinkerGraph graph;
 
 	private static String identifier;
+	
+	private static final String SUBJECT_LABEL = "ex:Picasso";
+	private static final String SUBJECT_EXPANDED_LABEL = "http://example.org/Picasso";
 
 	@Before
 	public void initializeGraph(){
@@ -73,9 +90,45 @@ public class RegistryDaoImplTest {
 
 	}
 	
+	
+	@Test
+	public void test_add_with_blank_node() throws Exception {
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		BNode address = vf.createBNode();
+		BNode painting = vf.createBNode();
+		BNode reaction = vf.createBNode();
+		ModelBuilder modelBuilder = createSimpleRDF(SUBJECT_LABEL);
+		editGraph(graph, modelBuilder.build());
+		modelBuilder = modelBuilder
+				.add(RDF.TYPE, "ex:Artist");
+		editGraph(graph, modelBuilder.build());
+//		test that a vertex got added since the object is an IRI
+		//assertEquals(2,countGraphVertices(graph));
+		modelBuilder = modelBuilder
+				.add(FOAF.DEPICTION, "ex:Image");
+		editGraph(graph, modelBuilder.build());
+		modelBuilder
+			.add("ex:homeAddress", address)
+			.add("ex:creatorOf", painting)
+			.subject(address)
+				.add("ex:street", "31 Art Gallery")
+				.add("ex:city", "Madrid")
+				.add("ex:country", "Spain")
+				.add(RDF.TYPE,"ex:PostalAddress")
+			.subject(painting)
+				.add(RDF.TYPE,"ex:CreativeWork")
+				.add("ex:depicts", "cubes")
+				.add("ex:reaction", reaction)
+				.subject(reaction)
+					.add("ex:rating","5")
+					.add(RDF.TYPE,"ex:AggregateRating");
+		editGraph(graph, modelBuilder.build());
+		boolean response = registryDao.addEntity(graph,SUBJECT_LABEL);
+		assertTrue(response);
+	}
+	
 	@Test
 	public void test_add_existing_entity(){
-		Vertex vertex = null;
 		String label = "teacher";
 		getVertexForSubject(label, "identifier", identifier);
 		getVertexForSubject(label, "type", "Teacher");
@@ -143,7 +196,7 @@ public class RegistryDaoImplTest {
 		identifier = generateRandomId();
 		Vertex firstVertex = null;
 		Vertex secondVertex = null;
-		String firstVertexLabel = "teacher3";
+		String firstVertexLabel = "teacher4";
 		String secondVertexLabel = "school";
 		getVertexForSubject(firstVertexLabel, "identifier", identifier);
 		firstVertex = getVertexForSubject(firstVertexLabel, "type", "Teacher");
@@ -244,6 +297,80 @@ public class RegistryDaoImplTest {
 		}
 		vertex.property(property, objectValue);
 		return vertex;
+	}
+	
+	private long countGraphVertices(Graph graph) {
+		return IteratorUtils.count(graph.vertices());
+	}
+
+	private void editGraph(Graph graph, Model simpleRDFModel) {
+		clearGraph(graph);
+		for(Statement rdfStatement: simpleRDFModel) {
+			convertRDFStatement2Graph(rdfStatement, graph);
+		}
+	}
+
+	private void clearGraph(Graph graph) {
+		graphTraversal(graph).drop().iterate();
+	}
+
+	private GraphTraversal<Vertex, Vertex> graphTraversal(Graph graph) {
+		return graph.traversal().V();
+	}
+
+	private void dumpGraph(Graph graph) throws IOException {
+		graph.io(IoCore.graphson()).writeGraph("dump.json");
+	}
+	
+	private ModelBuilder createSimpleRDF(String subjectLabel){
+		ModelBuilder builder = new ModelBuilder();
+		return builder
+				.setNamespace("ex", "http://example.org/")
+				.subject(subjectLabel)
+				.add(FOAF.FIRST_NAME, "Pablo");
+	}
+	
+	private Graph createGraph(){
+		Graph graph = TinkerGraph.open();
+		return graph;
+	}
+	
+	public static Graph convertRDFStatement2Graph(Statement rdfStatement, Graph graph) {
+		Value subjectValue = rdfStatement.getSubject();
+		IRI property = rdfStatement.getPredicate();
+		Value objectValue = rdfStatement.getObject();
+		updateGraph(subjectValue, property, objectValue, graph);
+		return graph;
+	}
+
+	private static void updateGraph(Value subjectValue, IRI property, Value objectValue, Graph graph) {
+		GraphTraversalSource t = graph.traversal();
+		GraphTraversal<Vertex, Vertex> hasLabel = t.V().hasLabel(subjectValue.toString());
+		Vertex s;
+		if(hasLabel.hasNext()){
+			s = hasLabel.next();
+		} else {
+			s = graph.addVertex(
+					T.label,
+					subjectValue.toString());
+		}
+		if (objectValue instanceof Literal) {
+			Literal literal = (Literal)objectValue;
+			s.property(property.toString(), literal.getLabel());
+
+		} else if (objectValue instanceof IRI) {
+			IRI objectIRI = (IRI)objectValue;
+			Vertex o = graph.addVertex(
+					T.label,
+					objectIRI.toString());
+			s.addEdge(property.toString(), o);
+		} else if (objectValue instanceof BNode) {
+			BNode objectBNode = (BNode)objectValue;
+			Vertex o = graph.addVertex(
+					T.label,
+					objectBNode.toString());
+			s.addEdge(property.toString(), o);
+		}
 	}
 
 }
