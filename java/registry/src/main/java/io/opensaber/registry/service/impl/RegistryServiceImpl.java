6ced711b1bdf0a85cb4341066c214c61a3844c14
Anand <anandp@ilimi.in>
2018-02-20 17:05:30 +0530

    Issue #60 feat: Refactor RegistryDaoImpl code

--- java/registry/src/main/java/io/opensaber/registry/service/impl/RegistryServiceImpl.java
+++ java/registry/src/main/java/io/opensaber/registry/service/impl/RegistryServiceImpl.java
@@ -4,16 +4,15 @@ import java.util.List;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
 
 import io.opensaber.registry.dao.RegistryDao;
 import io.opensaber.registry.exception.DuplicateRecordException;
@@ -22,14 +21,10 @@ import io.opensaber.registry.util.GraphDBFactory;
 import io.opensaber.utils.converters.RDF2Graph;
 import io.opensaber.registry.middleware.util.Constants;
 
-
-/**
- * 
- * @author jyotsna
- *
- */
 @Component
-public class RegistryServiceImpl implements RegistryService{
+public class RegistryServiceImpl implements RegistryService {
+
+	private static Logger logger = LoggerFactory.getLogger(RegistryServiceImpl.class);
 
 	@Autowired
 	RegistryDao registryDao;
@@ -43,31 +38,39 @@ public class RegistryServiceImpl implements RegistryService{
 	}
 
 	@Override
-	public boolean addEntity(Object entity) throws NullPointerException, DuplicateRecordException{
-		Graph graph = GraphDBFactory.getEmptyGraph();
-		Model rdfModel = (Model)entity;
-		StmtIterator iterator = rdfModel.listStatements();
-		boolean rootSubjectFound = false;
-		String label = null;
-		while(iterator.hasNext()){
-			Statement rdfStatement = iterator.nextStatement();
-			String type = environment.getProperty(Constants.SUBJECT_LABEL_TYPE);
-			String subjectValue = rdfStatement.getSubject().toString();
-			String predicate = rdfStatement.getPredicate().toString();
-			if(!rootSubjectFound && predicate.equals(RDF.TYPE.toString())){
-				RDFNode object = rdfStatement.getObject();
-				if(object.isURIResource()){
-					if(object.toString().equals(type)){
-						label = subjectValue;
-						rootSubjectFound = true;
-						System.out.println("Printing label:"+label);
+	public boolean addEntity(Object entity) throws NullPointerException, DuplicateRecordException {
+
+		try {
+			Graph graph = GraphDBFactory.getEmptyGraph();
+			Model rdfModel = (Model) entity;
+			StmtIterator iterator = rdfModel.listStatements();
+			boolean rootSubjectFound = false;
+			String label = null;
+
+			while (iterator.hasNext()) {
+				Statement rdfStatement = iterator.nextStatement();
+				String type = environment.getProperty(Constants.SUBJECT_LABEL_TYPE);
+				String subjectValue = rdfStatement.getSubject().toString();
+				String predicate = rdfStatement.getPredicate().toString();
+				if (!rootSubjectFound && predicate.equals(RDF.TYPE.toString())) {
+					RDFNode object = rdfStatement.getObject();
+					if (object.isURIResource()) {
+						if (object.toString().equals(type)) {
+							label = subjectValue;
+							rootSubjectFound = true;
+							System.out.println("Printing label:" + label);
+						}
 					}
 				}
+				graph = RDF2Graph.convertRDFStatement2Graph(rdfStatement, graph);
 			}
-			graph = RDF2Graph.convertRDFStatement2Graph(rdfStatement, graph);
-		}
 
-		return registryDao.addEntity(graph,label);
+			return registryDao.addEntity(graph, label);
+
+		} catch (Exception ex) {
+			logger.error("Error when creating entity in RegistryServiceImpl : ", ex);
+			throw ex;
+		}
 	}
 
 	@Override
