9a4ea92831dc52bd0770d182d8c2b3fdf841d16a
Shashank Teotia <shashankteotia@gmail.com>
2018-02-25 17:47:46 +0530

    Issue #20 feat: service and controller code for read

--- java/cukes/src/test/java/io/opensaber/registry/test/RegistryIntegrationSteps.java
+++ java/cukes/src/test/java/io/opensaber/registry/test/RegistryIntegrationSteps.java
@@ -4,19 +4,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+import io.opensaber.pojos.Response;
 
 
 /**
@@ -33,6 +38,7 @@ public class RegistryIntegrationSteps extends RegistryTestBase{
 	
 	private RestTemplate restTemplate;
 	private String baseUrl;
+	private ResponseEntity response;
 	private static String duplicateLabel;
 	
 	
@@ -43,115 +49,125 @@ public class RegistryIntegrationSteps extends RegistryTestBase{
 	}
 	
 	
-	@Given("^First input data and base url are valid")
+	@Given("^a valid record")
 	public void jsonldData(){
 		setJsonld(VALID_JSONLD);
-		assertNotNull(jsonld);
-		assertNotNull(restTemplate);
-		assertNotNull(baseUrl);
-	}
-	
-	@When("^Inserting first valid record into the registry")
-	public void addEntity(){
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
 		String label = generateRandomId();
-		duplicateLabel = label;
 		setJsonldWithNewRootLabel(CONTEXT_CONSTANT+label);
-		HttpEntity entity = new HttpEntity(jsonld,headers);
-		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
-				entity,ObjectNode.class);
-		ObjectNode obj = (ObjectNode)response.getBody();
-		assertNotNull(obj);
-		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), JsonKeys.SUCCESS);
+		assertNotNull(jsonld);
 	}
 	
-	@Then("^Response for first valid record is (.*)")
-	public void verifyResponse(String response){
-		assertNotNull(response);
-		assertTrue(response.contains(JsonKeys.SUCCESS));
+	@When("^issuing the record into the registry")
+	public void addEntity(){
+		response = callRegistryCreateAPI();
 	}
 
-	@Given("^Valid duplicate data")
-	public void jsonldDuplicateData(){
-		setJsonld(VALID_JSONLD);
-		assertNotNull(jsonld);
-		assertNotNull(restTemplate);
-		assertNotNull(baseUrl);
-	}
-	
-	@When("^Inserting a duplicate record into the registry")
-	public void addDuplicateEntity(){
+	private ResponseEntity callRegistryCreateAPI() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		setJsonldWithNewRootLabel(CONTEXT_CONSTANT+duplicateLabel);
 		HttpEntity entity = new HttpEntity(jsonld,headers);
-		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
-				entity,ObjectNode.class);
-		ObjectNode obj = (ObjectNode)response.getBody();
-		assertNotNull(obj);
-		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), Constants.DUPLICATE_RECORD_MESSAGE);
+		ResponseEntity response = restTemplate.postForEntity(
+				baseUrl+ADD_ENTITY,
+				entity,
+				ObjectNode.class);
+		return response;
 	}
 	
-	@Then("^Response for duplicate record is (.*)")
-	public void verifyFailureResponse(String response){
-		assertNotNull(response);
-		assertTrue(response.contains(Constants.DUPLICATE_RECORD_MESSAGE));
+	@Then("^record issuing should be successful")
+	public void verifyResponse() throws JsonParseException, JsonMappingException, IOException{
+		checkSuccessfulResponse();
+	}
+
+
+	private void checkSuccessfulResponse() throws JsonParseException, JsonMappingException, IOException {
+		String jsonString = response.getBody().toString();
+		Response responseObj = new ObjectMapper().readValue(jsonString, Response.class);
+		assertEquals(Response.Status.SUCCCESSFUL, responseObj.getParams().getStatus());
+//		Object obj = new JSONParser().parse(response.getBody());
+//		assertTrue(response.contains(JsonKeys.SUCCESSFUL));
 	}
 	
-	@Given("^Second input data and base url are valid")
-	public void newJsonldData(){
-		setJsonld(VALID_JSONLD);
-		assertNotNull(jsonld);
-		assertNotNull(restTemplate);
-		assertNotNull(baseUrl);
-	}
-	
-	@When("^Inserting second valid record into the registry")
-	public void addNewEntity(){
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		String label = generateRandomId();
-		setJsonldWithNewRootLabel(CONTEXT_CONSTANT + label);
-		HttpEntity entity = new HttpEntity(jsonld,headers);
-		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
-				entity,ObjectNode.class);
-		ObjectNode obj = (ObjectNode)response.getBody();
-		assertNotNull(obj);
-		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), JsonKeys.SUCCESS);
-	}
-	
-	@Then("^Response for second valid record is (.*)")
-	public void verifyResponse2(String response){
-		assertNotNull(response);
-		assertTrue(response.contains(JsonKeys.SUCCESS));
-	}
-	
-	@Given("^Base url is valid but input data has invalid type")
-	public void invalidJsonldData(){
-		setJsonld(INVALID_LABEL_JSONLD);
-		assertNotNull(jsonld);
-		assertNotNull(restTemplate);
-		assertNotNull(baseUrl);
-	}
-	
-	@When("^Inserting record with invalid type into the registry")
-	public void addInvalidEntity(){
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		/*String label = generateRandomId();
-		setJsonldWithNewRootLabel(label);*/
-		HttpEntity entity = new HttpEntity(jsonld,headers);
-		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
-				entity,ObjectNode.class);
-		ObjectNode obj = (ObjectNode)response.getBody();
-		assertNotNull(obj);
-		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), Constants.INVALID_TYPE_MESSAGE);
-	}
-	
-	@Then("^Response for invalid record is (.*)")
-	public void verifyFailureResponse2(String response){
-		assertNotNull(response);
-		assertTrue(response.contains(Constants.INVALID_TYPE_MESSAGE));
-	}
+//	@Given("^a record issued into the registry")
+//	public void prepareRegistryWithRecord() throws JsonParseException, JsonMappingException, IOException{
+//		jsonldData();
+//		addEntity();
+//		callRegistryCreateAPI();
+//		checkSuccessfulResponse();
+//	}
+//
+//	@Given("^Valid duplicate data")
+//	public void jsonldDuplicateData(){
+//		setJsonld(VALID_JSONLD);
+//		assertNotNull(jsonld);
+//		assertNotNull(restTemplate);
+//		assertNotNull(baseUrl);
+//	}
+//	
+//	@When("^Inserting a duplicate record into the registry")
+//	public void addDuplicateEntity(){
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.APPLICATION_JSON);
+//		setJsonldWithNewRootLabel(CONTEXT_CONSTANT+duplicateLabel);
+//		HttpEntity entity = new HttpEntity(jsonld,headers);
+//		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
+//				entity,ObjectNode.class);
+//		ObjectNode obj = (ObjectNode)response.getBody();
+//		assertNotNull(obj);
+//		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), Constants.DUPLICATE_RECORD_MESSAGE);
+//	}
+//	
+//	@Then("^Response for duplicate record is (.*)")
+//	public void verifyFailureResponse(String response){
+//		assertNotNull(response);
+//		assertTrue(response.contains(Constants.DUPLICATE_RECORD_MESSAGE));
+//	}
+//	
+//	@Given("^Second input data and base url are valid")
+//	public void newJsonldData(){
+//		setJsonld(VALID_JSONLD);
+//		assertNotNull(jsonld);
+//		assertNotNull(restTemplate);
+//		assertNotNull(baseUrl);
+//	}
+//	
+//	@When("^Inserting second valid record into the registry")
+//	public void addNewEntity(){
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.APPLICATION_JSON);
+//		String label = generateRandomId();
+//		setJsonldWithNewRootLabel(CONTEXT_CONSTANT + label);
+//		HttpEntity entity = new HttpEntity(jsonld,headers);
+//		ResponseEntity response = restTemplate.postForEntity(baseUrl+ADD_ENTITY,
+//				entity,ObjectNode.class);
+//		ObjectNode obj = (ObjectNode)response.getBody();
+//		assertNotNull(obj);
+//		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), JsonKeys.SUCCESSFUL);
+//	}
+//	
+//	@Then("^Response for second valid record is (.*)")
+//	public void verifyResponse2(String response){
+//		assertNotNull(response);
+//		assertTrue(response.contains(JsonKeys.SUCCESSFUL));
+//	}
+//	
+//	@Given("^Base url is valid but input data has invalid type")
+//	public void invalidJsonldData(){
+//		setJsonld(INVALID_LABEL_JSONLD);
+//		assertNotNull(jsonld);
+//		assertNotNull(restTemplate);
+//		assertNotNull(baseUrl);
+//	}
+//	
+//	@When("^Inserting record with invalid type into the registry")
+//	public void addInvalidEntity(){
+//		ResponseEntity obj = callRegistryCreateAPI();
+//		assertNotNull(obj);
+////		assertEquals(obj.get(JsonKeys.RESPONSE).asText(), Constants.INVALID_TYPE_MESSAGE);
+//	}
+//	
+//	@Then("^Response for invalid record is (.*)")
+//	public void verifyFailureResponse2(String response){
+//		assertNotNull(response);
+//		assertTrue(response.contains(Constants.INVALID_TYPE_MESSAGE));
+//	}
 }
--- java/cukes/src/test/java/io/opensaber/registry/test/create.feature
+++ java/cukes/src/test/java/io/opensaber/registry/test/create.feature
@@ -1,17 +1,17 @@
-#Author: jyotsna.raveendran@tarento.com
-
 @controller
 Feature: Inserting a record into the registry
 
-  Scenario: Inserting first valid record
-    Given First input data and base url are valid
-    When Inserting first valid record into the registry
-    Then Response for first valid record is success
+  Scenario: Issuing a valid record
+    Given a valid record
+    When issuing the record into the registry
+    Then record issuing should be successful
 
-  Scenario: Inserting a duplicate record
-    Given Valid duplicate data
-    When Inserting a duplicate record into the registry
-    Then Response for duplicate record is Cannot insert duplicate record
+  Scenario: Issuing a duplicate record
+    Given a record issued into the registry
+    And valid duplicate data
+    When Inserting the duplicate record into the registry
+    Then record issuing is successful
+    And response for duplicate record is Cannot insert duplicate record
 
   Scenario: Inserting second valid record into the registry
     Given Second input data and base url are valid
@@ -22,4 +22,4 @@ Feature: Inserting a record into the registry
     Given Base url is valid but input data has invalid type
     When Inserting record with invalid type into the registry
     Then Response for invalid record is Failed to insert due to invalid type
-    
+   
