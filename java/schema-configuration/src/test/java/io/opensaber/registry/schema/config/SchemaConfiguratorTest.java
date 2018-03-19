bce26f4ada76f0dc6575b1e823f089fde79845f6
jyotsna-tarento <jyotsna.raveendran@tarento.com>
2018-03-19 16:40:00 +0530

    Issue #120 test: adding test

--- java/schema-configuration/src/test/java/io/opensaber/registry/schema/config/SchemaConfiguratorTest.java
+++ java/schema-configuration/src/test/java/io/opensaber/registry/schema/config/SchemaConfiguratorTest.java
@@ -12,24 +12,32 @@ import io.opensaber.registry.schema.config.SchemaConfigurator;
 public class SchemaConfiguratorTest {
 	
 	private SchemaConfigurator schemaConfigurator;
-	private String configFileSchema = "opensaber-schema-configuration.jsonld";
+	private static final String CONFIG_SCHEMA_FILE = "opensaber-schema-configuration.jsonld";
 	
-	private void initialize() throws IOException{
-		schemaConfigurator = new SchemaConfigurator(configFileSchema);
+	private void initialize(String file) throws IOException{
+		schemaConfigurator = new SchemaConfigurator(file);
 	}
 
 	@Test
 	public void test_privacy_check_for_private_field() throws IOException{
-		initialize();
+		initialize(CONFIG_SCHEMA_FILE);
 		assertNotNull(schemaConfigurator.getSchemaConfig());
 		assertTrue(schemaConfigurator.isPrivate("http://example.com/voc/teacher/1.0.0/birthDate"));
 	}
 	
 	@Test
 	public void test_privacy_check_for_non_private_field() throws IOException{
-		initialize();
+		initialize(CONFIG_SCHEMA_FILE);
 		assertNotNull(schemaConfigurator.getSchemaConfig());
 		assertFalse(schemaConfigurator.isPrivate("http://example.com/voc/teacher/1.0.0/teacherName"));
 	}
+	
+	@Test
+	public void test_privacy_check_for_non_existent_field() throws IOException{
+		initialize(CONFIG_SCHEMA_FILE);
+		assertNotNull(schemaConfigurator.getSchemaConfig());
+		assertFalse(schemaConfigurator.isPrivate("sample"));
+	}
+
 
 }
