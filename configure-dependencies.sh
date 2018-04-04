ba125033c36f23b869cf5bf0c3357b145f5d556d
Anand <anandp@ilimi.in>
2018-04-04 23:56:06 +0530

    Issue #135 feat: Fix configuration dependencies initialization error

--- configure-dependencies.sh
+++ configure-dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Configuring dependencies before build"
 #cp java/registry/src/main/resources/config-test.properties.sample java/registry/src/main/resources/config-test.properties
+cp java/registry/src/main/resources/application.yml.sample java/registry/src/main/resources/application.yml
 cp java/registry/src/main/resources/schema-configuration-school-test.jsonld.sample java/registry/src/main/resources/schema-configuration-school-test.jsonld
 echo "Configuration of dependencies completed"
