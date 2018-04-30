a72c76d53f0d401460ab76c6f58a1c4dba8b774d
jyotsna-tarento <jyotsna.raveendran@tarento.com>
2018-04-30 21:45:09 +0530

    Issue #144 configuration

--- configure-dependencies.sh
+++ configure-dependencies.sh
@@ -3,4 +3,5 @@ echo "Configuring dependencies before build"
 #cp java/registry/src/main/resources/config-test.properties.sample java/registry/src/main/resources/config-test.properties
 cp java/registry/src/main/resources/application.yml.sample java/registry/src/main/resources/application.yml
 cp java/registry/src/main/resources/schema-configuration-school-test.jsonld.sample java/registry/src/main/resources/schema-configuration-school-test.jsonld
+cp java/schema-configuration/src/main/resources/validations.shex.sample java/schema-configuration/src/main/resources/validations.shex
 echo "Configuration of dependencies completed"
