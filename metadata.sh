5626391705d7432e5eb0ee94bdd1d8b2750908fb
jyotsna-tarento <jyotsna.raveendran@tarento.com>
2018-06-26 12:11:44 +0530

    Issue #SB-4880:configuration changes

--- metadata.sh
+++ metadata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Return service metadata
-echo '{"name":"registry","version":"1.0.0","org":"opensaber","hubuser":"purplesunbird"}'
+echo '{"name":"registry","version":"1.0.0","org":"opensaber","hubuser":""}'
