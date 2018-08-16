0e32513a56af4723a0035a63476fe1c55a73bfac
jyotsna-tarento <jyotsna.raveendran@tarento.com>
2018-08-16 15:47:38 +0530

    Issue OS-7: changes based on review comments

--- java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/SearchUtil.java
+++ java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/SearchUtil.java
@@ -19,7 +19,7 @@ import io.opensaber.pojos.SearchQuery;
 public class SearchUtil {
 
 	private static Logger logger = LoggerFactory.getLogger(SearchUtil.class);
-	
+
 	public static SearchQuery constructSearchQuery(Model inputRdf){
 		StmtIterator stmtIterator = inputRdf.listStatements();
 		List<Filter> filterList = new ArrayList<Filter>();
@@ -28,53 +28,54 @@ public class SearchUtil {
 			Statement statement = stmtIterator.next();
 			Resource resource = statement.getSubject();			
 			Property predicate = statement.getPredicate();
-			RDFNode rdfNode = statement.getObject();
-			List<String> path = new ArrayList<String>();
-			Object object = null;
-			if(!rdfNode.isAnon()){
-				if(rdfNode.isLiteral()){
-					object = rdfNode.asLiteral().getLexicalForm();
-				}else{
-					object = rdfNode.toString();
-				}
-				if(predicate.equals(RDF.type)){
-					searchQuery.setTypeIRI(RDF.type.toString());
-					searchQuery.setType((String)object);
-				}else{
-					Filter filter = new Filter();
-					filter.setSubject(resource.toString());
-					filter.setProperty(predicate.toString());
-					if(filterList.contains(filter)){
-						filter = filterList.get(filterList.indexOf(filter));
-						if(filter.getValue()!=null){
-							Object o = filter.getValue();
-							List objList = null;
-							if(o instanceof List){
-								objList = (List)o;
-							}else{
-								objList = new ArrayList();
-								objList.add(object);
-							}
-							objList.add(o);
-							filter.setValue(objList);
-						}else{
-							filter.setValue(object);
-						}
-						
-					}else{
-						path = getNestedPath(inputRdf,resource, path);
-						filter.setPath(path);
-						filter.setValue(object);
-						filterList.add(filter);
-					}
-				}
+			RDFNode rdfNode = statement.getObject();	
+			if(predicate.equals(RDF.type)){
+				searchQuery.setTypeIRI(RDF.type.toString());
+				searchQuery.setType(rdfNode.toString());
+			} else if(!rdfNode.isAnon()){
+				filterList = updateFilterList(inputRdf, resource, predicate, rdfNode, filterList);
 			}
+			//else we are ignoring anonymous rdf nodes
 		}
-		logger.info("Filter list for search query:"+filterList);
+		logger.info(String.format("Number of filters applied: %d",filterList.size()));
+		logger.info(String.format("Filter list for search query : %s",filterList));
 		searchQuery.setFilters(filterList);
 		return searchQuery;
 	}
-	
+
+	private static List<Filter> updateFilterList(Model inputRdf, Resource resource, Property predicate,
+			RDFNode rdfNode, List<Filter> filterList){
+		Object object = null;
+		List<String> path = new ArrayList<String>();
+		if(rdfNode.isLiteral()){
+			object = rdfNode.asLiteral().getLexicalForm();
+		}else{
+			object = rdfNode.toString();
+		}
+		Filter filter = new Filter();
+		filter.setSubject(resource.toString());
+		filter.setProperty(predicate.toString());
+		if(filterList.contains(filter)){
+			filter = filterList.get(filterList.indexOf(filter));
+			Object o = filter.getValue();
+			List objList = null;
+			if(o instanceof List){
+				objList = (List)o;
+			}else{
+				objList = new ArrayList();
+				objList.add(o);
+			}
+			objList.add(object);
+			filter.setValue(objList);
+		}else{
+			path = getNestedPath(inputRdf,resource, path);
+			filter.setPath(path);
+			filter.setValue(object);
+			filterList.add(filter);
+		}
+		return filterList;
+	}
+
 	private static List<String> getNestedPath(Model inputRdf, Resource resource, List<String> path){
 		StmtIterator stmtIter = inputRdf.listStatements(null, null, resource);
 		while(stmtIter.hasNext()){
