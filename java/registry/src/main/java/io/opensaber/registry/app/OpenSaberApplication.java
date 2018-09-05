d608b5cccc09614d0f7f62fff9c646f182e7cb79
indrajra-ilimi <rajeshr@ilimi.in>
2018-09-05 14:44:39 +0530

    Issue #OS-58 Enable toggle for relevant signature service impl

--- java/registry/src/main/java/io/opensaber/registry/app/OpenSaberApplication.java
+++ java/registry/src/main/java/io/opensaber/registry/app/OpenSaberApplication.java
@@ -3,9 +3,13 @@ package io.opensaber.registry.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @ComponentScan({"io.opensaber.registry"})
+@Configuration
+@EnableRetry
 public class OpenSaberApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpenSaberApplication.class, args);
---
+++ java/registry/src/main/java/io/opensaber/registry/exception/SignatureException.java
@@ -0,0 +1,29 @@
+package io.opensaber.registry.exception;
+
+public class SignatureException extends Exception {
+    public class CreationException extends CustomException {
+        private static final long serialVersionUID = 6174717850058203376L;
+
+        public CreationException(String msg) {
+            super("Unable to create signature: " + msg);
+        }
+    }
+
+    public class VerificationException extends CustomException {
+
+        private static final long serialVersionUID = 4996784337180620650L;
+
+        public VerificationException(String message) {
+            super("Unable to verify signature: " + message);
+        }
+    }
+
+    public class UnreachableException extends CustomException {
+
+        private static final long serialVersionUID = 5384120386096139083L;
+
+        public UnreachableException(String message) {
+            super("Unable to reach service: " + message);
+        }
+    }
+}
