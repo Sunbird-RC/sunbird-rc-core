8166a83d58381bc215217d9ead6fe197ab01cd6c
indrajra-ilimi <rajeshr@ilimi.in>
2018-09-17 12:08:06 +0530

    Issue #OS-58 Remove configuration annotation

--- java/registry/src/main/java/io/opensaber/registry/app/OpenSaberApplication.java
+++ java/registry/src/main/java/io/opensaber/registry/app/OpenSaberApplication.java
@@ -3,11 +3,9 @@ package io.opensaber.registry.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan({"io.opensaber.registry"})
-@Configuration
 public class OpenSaberApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpenSaberApplication.class, args);
