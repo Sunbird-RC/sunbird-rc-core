f68283a7321dc2c4f4fcfc2ed2a9969d4e361734
indrajra-ilimi <rajeshr@ilimi.in>
2018-09-19 15:17:57 +0530

    Issue #OS-46 Add models for registry signature

--- java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/Constants.java
+++ java/middleware-commons/src/main/java/io/opensaber/registry/middleware/util/Constants.java
@@ -64,10 +64,17 @@ public class Constants {
 	public static final String READ_METHOD_ORIGIN = "read";
 	public static final String UPDATE_METHOD_ORIGIN = "update";
 	public static final String SEARCH_METHOD_ORIGIN = "search";
-	public static final String SIGNATURES = "signatures";
+    public static final String FORWARD_SLASH = "/";
+
+    // List of predicates introduced for digital signature.
+    public static final String SIGNATURES = "signatures";
 	public static final String SIGNATURE_OF = "signatureOf";
 	public static final String SIGNATURE_FOR = "signatureFor";
-	public static final String FORWARD_SLASH = "/";
+    public static final String SIGN_CREATOR = "creator";
+    public static final String SIGN_CREATED_TIMESTAMP = "created";
+    public static final String SIGN_NONCE = "nonce";
+    public static final String SIGN_TYPE = "type";
+    public static final String SIGN_SIGNATURE_VALUE = "signatureValue";
 
 	public enum GraphDatabaseProvider {
 		NEO4J("NEO4J"),
