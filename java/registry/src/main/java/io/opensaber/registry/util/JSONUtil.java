d33f23adf5cfb3050794372d982abc5edc9bce4c
jyotsna-tarento <jyotsna.raveendran@tarento.com>
2018-09-21 00:36:48 +0530

    Issue #OS-46:method to remove id from claim

--- java/registry/src/main/java/io/opensaber/registry/util/JSONUtil.java
+++ java/registry/src/main/java/io/opensaber/registry/util/JSONUtil.java
@@ -5,6 +5,10 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 public class JSONUtil {
     private static Type type = new TypeToken<Map<String, Object>>(){}.getType();
@@ -14,5 +18,12 @@ public class JSONUtil {
         String result = gson.toJson(object);
         return gson.fromJson(result, type);
     }
+    
+    public static String getModifiedClaim(String payload){
+		String value = "\"@id\":\"[a-z]+:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\",";
+		Pattern pattern = Pattern.compile(value);
+		Matcher matcher = pattern.matcher(payload);
+		return matcher.replaceAll(StringUtils.EMPTY);
+	}
 
 }
