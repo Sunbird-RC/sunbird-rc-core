e39145ed24c95dd4c0fff2299fec59f6cc75df89
pritha-tarento <pritha.chattopadhyay@tarento.com>
2018-10-25 12:48:05 +0530

    Issue #OS-95 Cleaned up files

--- java/schema-configuration/src/main/java/io/opensaber/registry/schema/config/SchemaLoader.java
+++ java/schema-configuration/src/main/java/io/opensaber/registry/schema/config/SchemaLoader.java
@@ -19,16 +19,16 @@ public class SchemaLoader {
 	private Schema schemaForUpdate;
 	private Model validationConfig;
 	private static final String SCHEMAFORMAT = "SHEXC";
-	private static final String PROCESSOR 	= "shex";
+	private static final String PROCESSOR = "shex";
 	private static final String JSON_LD = "JSON-LD";
-	
-	public SchemaLoader(String validationcreateFile, String validationUpdateFile) throws IOException{
+
+	public SchemaLoader(String validationcreateFile, String validationUpdateFile) throws IOException {
 
 		schemaForCreate = loadSchemaForValidation(validationcreateFile);
 		schemaForUpdate = loadSchemaForValidation(validationUpdateFile);
 		validationConfig = loadValidationConfigModel();
 	}
-	
+
 	private Schema loadSchemaForValidation(String validationFile) throws IOException {
 		InputStream is = this.getClass().getClassLoader().getResourceAsStream(validationFile);
 		if (is == null) {
@@ -42,8 +42,8 @@ public class SchemaLoader {
 	private Model loadValidationConfigModel() {
 		return RDFUtil.getRdfModelBasedOnFormat(schemaForUpdate.serialize(JSON_LD).right().get(), JSON_LD);
 	}
-	
-	public Model getValidationConfig(){
+
+	public Model getValidationConfig() {
 		return validationConfig;
 	}
 
--- java/schema-configuration/src/test/java/io/opensaber/registry/schema/config/SchemaConfiguratorTest.java
+++ java/schema-configuration/src/test/java/io/opensaber/registry/schema/config/SchemaConfiguratorTest.java
@@ -10,74 +10,72 @@ import org.junit.Test;
 
 import io.opensaber.registry.schema.config.SchemaConfigurator;
 
-
 public class SchemaConfiguratorTest {
-	
+
 	private SchemaConfigurator schemaConfigurator;
 	private static final String CONFIG_SCHEMA_FILE = "opensaber-schema-configuration.jsonld";
 	private static final String CONFIG_VALIDATION_FILE = "validations.shex";
 	private SchemaLoader schemaLoader;
-	
-	private void initialize(String file, String validationFile) throws IOException{
+
+	private void initialize(String file, String validationFile) throws IOException {
 		schemaLoader = new SchemaLoader(validationFile, validationFile);
-		schemaConfigurator = new SchemaConfigurator(file, "http://example.com/voc/opensaber/1.0.0/",schemaLoader);
+		schemaConfigurator = new SchemaConfigurator(file, "http://example.com/voc/opensaber/1.0.0/", schemaLoader);
 	}
 
 	@Test
-	public void test_privacy_check_for_private_field() throws IOException{
-		
+	public void test_privacy_check_for_private_field() throws IOException {
+
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
 		assertNotNull(schemaConfigurator.getSchemaConfig());
 		assertTrue(schemaConfigurator.isPrivate("http://example.com/voc/teacher/1.0.0/birthDate"));
 	}
-	
+
 	@Test
-	public void test_privacy_check_for_non_private_field() throws IOException{
+	public void test_privacy_check_for_non_private_field() throws IOException {
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
 		assertNotNull(schemaConfigurator.getSchemaConfig());
 		assertFalse(schemaConfigurator.isPrivate("http://example.com/voc/teacher/1.0.0/teacherName"));
 	}
-	
+
 	@Test
-	public void test_privacy_check_for_non_existent_field() throws IOException{
+	public void test_privacy_check_for_non_existent_field() throws IOException {
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
 		assertNotNull(schemaConfigurator.getSchemaConfig());
 		assertFalse(schemaConfigurator.isPrivate("sample"));
 	}
-	
+
 	@Test
-	public void test_single_valued_property() throws IOException{
+	public void test_single_valued_property() throws IOException {
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
-		//assertNotNull(schemaConfigurator.getValidationConfig());
+		// assertNotNull(schemaConfigurator.getValidationConfig());
 		assertTrue(schemaConfigurator.isSingleValued("http://example.com/voc/teacher/1.0.0/serialNum"));
 	}
-	
+
 	@Test
-	public void test_multi_valued_property() throws IOException{
+	public void test_multi_valued_property() throws IOException {
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
 		assertNotNull(schemaLoader.getValidationConfig());
 		assertFalse(schemaConfigurator.isSingleValued("http://example.com/voc/teacher/1.0.0/mainSubjectsTaught"));
 	}
-	
+
 	@Test
-	public void test_non_configured_property() throws IOException{
+	public void test_non_configured_property() throws IOException {
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
 		assertNotNull(schemaLoader.getValidationConfig());
 		assertTrue(schemaConfigurator.isSingleValued("http://example.com/voc/teacher/1.0.0/predicate"));
 	}
-	
+
 	@Test
 	public void test_isEncrypted() throws Exception {
 		byte[] array = new byte[7];
 		new Random().nextBytes(array);
 		String randomString = new String(array, Charset.forName("UTF-8"));
-		String encryptedProperty= "encryptedschoolName";
+		String encryptedProperty = "encryptedschoolName";
 		initialize(CONFIG_SCHEMA_FILE, CONFIG_VALIDATION_FILE);
-		
-		assertEquals(true,schemaConfigurator.isEncrypted(encryptedProperty));
-		assertEquals(false,schemaConfigurator.isEncrypted(null));
-		assertEquals(false,schemaConfigurator.isEncrypted(randomString));
-	}
 
+		assertEquals(true, schemaConfigurator.isEncrypted(encryptedProperty));
+		assertEquals(false, schemaConfigurator.isEncrypted(null));
+		assertEquals(false, schemaConfigurator.isEncrypted(randomString));
+	}
 
 }
