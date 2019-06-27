64818cdfa80d6ccc26bf7e45b4d38a5020a4663b
indrajra-ilimi <rajeshr@ilimi.in>
2019-06-27 22:19:09 +0530

    Add samples for demo

---
+++ ui-sample/utils.js
@@ -0,0 +1,20 @@
+function dataLocalizeRefresh(lang, fullLangText) {
+  var opts = { language: lang, pathPrefix: "./lang" };
+  $("[data-localize]").localize("stringData", opts);
+  console.log("dataLocalizeRefreshEnd " + lang);
+  document.getElementById("langSelected").innerHTML = fullLangText
+}
+
+function dataLocalizeRefreshDefault() {
+  var langSelected = document.getElementById("langSelected").innerText
+  console.log(langSelected)
+  var lang = "en"
+  if (langSelected != null) {
+    lang = langSelected.innerText
+    console.log('lo', langSelected, lang)
+  }
+  var opts = { language: lang, pathPrefix: "./lang" };
+  $("[data-localize]").localize("stringData", opts);
+  console.log("dataLocalizeRefreshEnd " + lang);
+
+}
