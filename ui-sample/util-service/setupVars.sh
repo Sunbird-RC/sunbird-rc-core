08e33ae064ece26b76bb1ffc0eea465d916c253f
Aishwarya <42641684+aishwa8141@users.noreply.github.com>
2019-11-25 18:02:02 +0530

    Moved role config and url config into app.config.json (#332)
    * Moved urlConfig and rolesConfig to app.config.json
    
    * Pofile page get edit form of owner role
    
    * Changed env variable name in app.js

--- ui-sample/util-service/setupVars.sh
+++ ui-sample/util-service/setupVars.sh
@@ -1,3 +1,3 @@
 export registry_url="http://localhost:8081"
 export keycloak_realmName="PartnerRegistry"
-export keyCloak_url="http://localhost:8080/auth/admin/realms/"
+export keycloak_url="http://localhost:8080/auth/admin/realms/"
