61eaa77d9aa6f2f8b251e1b05aa4660e13a25338
kesavanpalani <kesavanpalanipk@gmail.com>
2021-05-12 15:11:39 +0530

    Setup claims module

---
+++ java/claims/src/main/java/io/opensaber/claims/ClaimsApplication.java
@@ -0,0 +1,13 @@
+package io.opensaber.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsApplication.class, args);
+	}
+
+}
---
+++ java/claims/src/test/java/io/opensaber/claims/ClaimsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.opensaber.claims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
