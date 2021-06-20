7f4051597a10e4c7491875e82f31f48912695655
Sai Sathvik <saisathviky@sahaj.ai>
2021-06-20 20:45:07 +0530

    make EntityPropertyURI from node & propertyURI

--- java/registry/src/main/java/io/opensaber/registry/model/attestation/EntityPropertyURI.java
+++ java/registry/src/main/java/io/opensaber/registry/model/attestation/EntityPropertyURI.java
@@ -1,6 +1,10 @@
 package io.opensaber.registry.model.attestation;
 
 import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.util.Optional;
 
 public class EntityPropertyURI {
     String propertyURI;
@@ -26,5 +30,39 @@ public class EntityPropertyURI {
         merged.propertyURI = m1.propertyURI + uuidPath;
         merged.jsonPointer = JsonPointer.compile(m1.jsonPointer.toString() + jsonPath);
         return merged;
+    }
+
+    public static Optional<EntityPropertyURI> fromEntityAndPropertyURI(JsonNode node, String propertyURI, String uuidPropertyName) {
+        String[] steps = (propertyURI.startsWith("/") ? propertyURI.substring(1) : propertyURI).split("/");
+        JsonNode curr = node;
+        for (int i = 0; i < steps.length; i++) {
+            if (curr == null || curr.isMissingNode()) {
+                return Optional.empty();
+            }
+            if (!curr.isArray()) {
+                curr = curr.get(steps[i]);
+                continue;
+            }
+            int index = -1;
+            try {
+                index = Integer.parseInt(steps[i]);
+            } catch (NumberFormatException nfe) {
+                ArrayNode arrNode = (ArrayNode)curr;
+                for (int j = 0; j < arrNode.size(); j++) {
+                    if (arrNode.get(j).get(uuidPropertyName).asText().equals(steps[i])) {
+                        steps[i] = String.valueOf(j);
+                        index = j;
+                    }
+                }
+            }
+            if (index == -1) {
+                return Optional.empty();
+            }
+            curr = curr.get(index);
+        }
+        return Optional.of(new EntityPropertyURI(
+                propertyURI,
+                "/" + String.join("/", steps)
+        ));
     }
 }
