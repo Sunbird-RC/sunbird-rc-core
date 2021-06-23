f8a6eb72d99728d9978547736641b14dc9bf6bcb
Sai Sathvik <saisathviky@sahaj.ai>
2021-06-23 09:34:14 +0530

    entityStateHelper tests

---
+++ java/registry/src/test/java/io/opensaber/registry/helper/EntityStateHelperTestConfiguration.java
@@ -0,0 +1,35 @@
+package io.opensaber.registry.helper;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.runtime.KieContainer;
+import org.kie.internal.io.ResourceFactory;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+
+@TestConfiguration
+public class EntityStateHelperTestConfiguration {
+
+    @Bean
+    RestTemplate mockRestTemplate() {
+        return  Mockito.mock(RestTemplate.class);
+    }
+
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+        kieFileSystem.write(ResourceFactory.newFileResource(new File("src/main/resources/workflow/statetransitions.drl")));
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        kieBuilder.buildAll();
+        KieModule kieModule = kieBuilder.getKieModule();
+        return kieServices.newKieContainer(kieModule.getReleaseId());
+    }
+
+}
