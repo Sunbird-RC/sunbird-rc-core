55b2edb5d308c71648d60ed1bcc993b68dde5255
Dileep Bapat <dileepbapat@gmail.com>
2021-08-12 13:17:22 +0530

    Adding basic test runner

---
+++ java/apitest/src/test/java/karate-config.js
@@ -0,0 +1,18 @@
+function fn() {
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    env: env,
+    baseurl: 'http://localhost:8081'
+  }
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+  } else if (env == 'e2e') {
+    // customize
+  }
+  return config;
+}
