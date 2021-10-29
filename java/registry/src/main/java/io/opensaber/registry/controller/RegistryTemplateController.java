fafbb9c30cb4747f2e0f8ea2e6c8384b5c719d44
kesavanp123 <kesavanp@sahaj.ai>
2021-10-29 10:08:54 +0530

    Expose API to fetch template

---
+++ java/registry/src/main/java/io/opensaber/registry/controller/RegistryTemplateController.java
@@ -0,0 +1,28 @@
+package io.opensaber.registry.controller;
+
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@RestController
+public class RegistryTemplateController{
+    @RequestMapping(value = "/api/v1/templates/{fileName}.html", method = RequestMethod.GET)
+    public String getTemplate(@PathVariable String fileName) {
+        String content = "";
+        try {
+            String templatesFolderPath = "public/_schemas/templates/";
+            File file = new ClassPathResource(templatesFolderPath + fileName + ".html").getFile();
+            content = new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+}
