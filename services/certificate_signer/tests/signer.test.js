c22149361367ef5515c749ab323d19f096281751
Tejash <tejash.jl1020@gmail.com>
2021-11-03 16:43:22 +0530

    Added credential signing service

---
+++ services/certificate_signer/tests/signer.test.js
@@ -0,0 +1,21 @@
+var Mustache = require('mustache');
+test('Should render json template', async () => {
+    const entity = {
+      identityDetails: {
+          name: "Tejash"
+      }
+    };
+    const template = {
+        "@context": ["https://www.w3.org/2018/credentials/v1"],
+        "type": ["VerifiableCredential"],
+        "credentialSubject": "Person",
+        "issuanceDate": "2021-08-27T10:57:57.237Z",
+        "data": {
+            "name": "{{identityDetails.name}}"
+        },
+        "issuer": "did:issuer:sunbird",
+        // "date": "28-09-2021",
+    }
+    const output = JSON.parse(Mustache.render(JSON.stringify(template), entity));
+    expect(output.data.name).toBe("Tejash");
+});
