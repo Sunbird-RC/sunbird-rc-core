10830d1cb9bca906a22d3bbfd085f01b849784c7
Dileep Bapat <dileepbapat@gmail.com>
2021-11-09 23:27:12 +0530

    Certificate api docker-compose file and tested few negative scenarios

--- services/certificate-api/main.js
+++ services/certificate-api/main.js
@@ -5,9 +5,19 @@ const port = process.env.PORT || 4321;
 const server = http.createServer(async (req, res) => {
     console.time(req.url)
     console.log(`API ${req.method} ${req.url} called`);
-    if (req.method === 'POST' && req.url.startsWith("/api/v1/certificatePDF")) {
-        const data = await certificateController.getCertificatePDF(req, res);
-        res.end(data)
+    try {
+        if (req.method === 'POST' && req.url.startsWith("/api/v1/certificatePDF")) {
+            const data = await certificateController.getCertificatePDF(req, res);
+            res.end(data)
+        } else {
+            res.statusCode = 404;
+            res.end("Not found");
+        }
+    } finally {
+        if (!res.writableEnded) {
+            res.statusCode = 500;
+            res.end("Error occurred");
+        }
     }
     console.timeEnd(req.url)
 });
