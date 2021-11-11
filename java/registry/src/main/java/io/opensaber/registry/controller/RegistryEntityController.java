2bb3e90938d3aa4257e851fd13ef95e42d4a1870
kesavanp123 <kesavanp@sahaj.ai>
2021-11-11 14:56:16 +0530

    Expose certificate api from regi

--- java/registry/src/main/java/io/opensaber/registry/controller/RegistryEntityController.java
+++ java/registry/src/main/java/io/opensaber/registry/controller/RegistryEntityController.java
@@ -14,6 +14,7 @@ import io.opensaber.registry.middleware.MiddlewareHaltException;
 import io.opensaber.registry.middleware.util.Constants;
 import io.opensaber.registry.middleware.util.JSONUtil;
 import io.opensaber.registry.middleware.util.OSSystemFields;
+import io.opensaber.registry.service.ICertificateService;
 import io.opensaber.registry.service.SignatureService;
 import io.opensaber.registry.transform.Configuration;
 import io.opensaber.registry.transform.Data;
@@ -42,6 +43,9 @@ public class RegistryEntityController extends AbstractController {
 
     @Autowired
     private SignatureService signatureService;
+
+    @Autowired
+    private ICertificateService certificateService;
 
     @Value("${authentication.enabled:true}") boolean authenticationEnabled;
 
@@ -381,8 +385,19 @@ public class RegistryEntityController extends AbstractController {
     }
 
     @RequestMapping(value = "/api/v1/{entityName}/{entityId}", method = RequestMethod.GET, produces = {"application/pdf"})
-    public ResponseEntity<Object> getEntityType(){
-        return new ResponseEntity<>("", HttpStatus.OK);
+    public ResponseEntity<Object> getEntityType(@PathVariable String entityName,
+                                                @PathVariable String entityId,
+                                                HttpServletRequest request) {
+        try {
+            String readerUserId = getUserId(request);
+            JsonNode node = registryHelper.readEntity(readerUserId, entityName, entityId, false, null, false)
+                    .get(entityName);
+            node = objectMapper.readTree(node.get(OSSystemFields._osSignedData.name()).asText());
+            return new ResponseEntity<>(certificateService.getPdf(node), HttpStatus.BAD_REQUEST);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
     @RequestMapping(value = "/api/v1/{entityName}/{entityId}", method = RequestMethod.GET)
     public ResponseEntity<Object> getEntity(
@@ -413,7 +428,7 @@ public class RegistryEntityController extends AbstractController {
         ResponseParams responseParams = new ResponseParams();
         Response response = new Response(Response.API_ID.READ, "OK", responseParams);
         try {
-            String readerUserId = authenticationEnabled ? registryHelper.getUserId(request) : io.opensaber.registry.Constants.USER_ANONYMOUS;
+            String readerUserId = getUserId(request);
             JsonNode node = getEntityJsonNode(entityName, entityId, requireLDResponse, readerUserId);
             if(requireLDResponse) {
                 addJsonLDSpec(node);
@@ -433,6 +448,10 @@ public class RegistryEntityController extends AbstractController {
             responseParams.setStatus(Response.Status.UNSUCCESSFUL);
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private String getUserId(HttpServletRequest request) throws Exception {
+        return authenticationEnabled ? registryHelper.getUserId(request) : io.opensaber.registry.Constants.USER_ANONYMOUS;
     }
 
     private void addJsonLDSpec(JsonNode node) {
--- java/registry/src/main/java/io/opensaber/registry/helper/RegistryHelper.java
+++ java/registry/src/main/java/io/opensaber/registry/helper/RegistryHelper.java
@@ -35,6 +35,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
