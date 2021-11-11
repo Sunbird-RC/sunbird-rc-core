eef5e8e74251449a570e7ebad68b26acba4096e9
Dileep Bapat <dileepbapat@gmail.com>
2021-11-11 16:26:24 +0530

    Adding logs for build failure and timeout

--- build/wait_for_port.sh
+++ build/wait_for_port.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Waiting service to launch on $1..."
-
+i=0
 while ! curl localhost:$1; do
-  sleep 1
+  sleep 10
+  let i=i+1
+  if [[ $i -gt 60 ]]; then
+    echo "Failed to get the service in sane state!"
+    exit 1;
+  fi
 done
 
 echo "$1 port accessible"
