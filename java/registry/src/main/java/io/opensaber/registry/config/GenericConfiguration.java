91c670b225c9c7e8e8dc24778dd64a698afe8af1
Tejash <tejash.jl1020@gmail.com>
2021-12-15 14:02:38 +0530

    extracted hardcoded configs to env

--- java/registry/src/main/java/io/opensaber/registry/config/GenericConfiguration.java
+++ java/registry/src/main/java/io/opensaber/registry/config/GenericConfiguration.java
@@ -153,9 +153,6 @@ public class GenericConfiguration implements WebMvcConfigurer {
 	@Value("${registry.schema.url}")
 	private String schemaUrl;
 
-	private String PRIVATE_KEY = "984b589e121040156838303f107e13150be4a80fc5088ccba0b0bdc9b1d89090de8777a28f8da1a74e7a13090ed974d879bf692d001cddee16e4cc9f84b60580";
-	private String PUBLIC_KEY = "de8777a28f8da1a74e7a13090ed974d879bf692d001cddee16e4cc9f84b60580";
-
 	static {
 		Config config = ConfigFactory.parseResources("opensaber-actors.conf");
 
