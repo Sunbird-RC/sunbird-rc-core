8d825554410c7fe54cd14790429f49c0748ce1fb
Suraj Shanbhag <surajs@sahaj.ai>
2022-02-25 09:32:24 +0530

    Add Delete Entity Api and Respond with 401 in case of Unauthorised operation

---
+++ java/registry/src/main/java/dev/sunbirdrc/registry/exception/UnAuthorizedException.java
@@ -0,0 +1,7 @@
+package dev.sunbirdrc.registry.exception;
+
+public class UnAuthorizedException extends RuntimeException{
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+}
