e2fee98cc4399730f68b848fec61e253f2eac3b5
Tejash <tejash.jl1020@gmail.com>
2022-05-23 08:22:49 +0530

    added anonymous role support for post entity api

--- java/registry/src/main/java/dev/sunbirdrc/registry/helper/RegistryHelper.java
+++ java/registry/src/main/java/dev/sunbirdrc/registry/helper/RegistryHelper.java
@@ -766,6 +766,9 @@ public class RegistryHelper {
 
         List<String> managingRoles = getManageRoles(entityName);
         if (managingRoles.size() > 0) {
+            if (managingRoles.contains(ROLE_ANONYMOUS)) {
+                return ROLE_ANONYMOUS;
+            }
             Set<String> userRoles = getUserRolesFromRequest(request);
             authorizeUserRole(userRoles, managingRoles);
             return fetchUserIdFromToken(request);
