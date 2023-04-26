e38c69f148c090f5e115f6d097ad1092bf6d2638
Tejas Varade <varadetejas@gmail.com>
2023-04-26 14:58:13 +0530

    Add default constructors to NotificationTemplate and NotificationTemplates models
    This will help in setting notification template values from schemas

--- java/registry/src/main/java/dev/sunbirdrc/registry/model/NotificationTemplate.java
+++ java/registry/src/main/java/dev/sunbirdrc/registry/model/NotificationTemplate.java
@@ -2,9 +2,11 @@ package dev.sunbirdrc.registry.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationTemplate {
     private String subject;
     private String body;
--- java/registry/src/main/java/dev/sunbirdrc/registry/model/NotificationTemplates.java
+++ java/registry/src/main/java/dev/sunbirdrc/registry/model/NotificationTemplates.java
@@ -1,8 +1,10 @@
 package dev.sunbirdrc.registry.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructorjava/registry/src/main/java/dev/sunbirdrc/registry/model/NotificationTemplates.java
 public class NotificationTemplates {
     private NotificationTemplate createNotificationTemplates = new NotificationTemplate("Credential Created", "{{name}}, Your {{entityType}} credential has been created");
     private NotificationTemplate updateNotificationTemplates = new NotificationTemplate("Credential Updated", "{{name}}, Your {{entityType}} credential has been updated");
