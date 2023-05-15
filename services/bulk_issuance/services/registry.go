f51f6e143792798847499bdb34340fe2545238f8
Tejash <tejash.jl1020@gmail.com>
2023-05-15 22:30:46 +0530

    - update get sample error response to json format - update parsing schema properties from map to struct

--- services/bulk_issuance/services/registry.go
+++ services/bulk_issuance/services/registry.go
@@ -5,13 +5,14 @@ import (
 	"bulk_issuance/utils"
 	"encoding/json"
 	"errors"
+	"github.com/go-openapi/spec"
 	"io"
 	"net/http"
 	"sort"
 )
 
 func GetSchemaProperties(key string) ([]string, error) {
-	schemaProperties, err := getJsonSchemaProperties(key)
+	schemaProperties, err := getSchemaProperties(key)
 	if err == nil {
 		properties := make([]string, 0)
 		for k := range schemaProperties {
@@ -24,31 +25,35 @@ func GetSchemaProperties(key string) ([]string, error) {
 	}
 }
 
-func getSchemaPropertiesAndSampleValues(key string) ([]string, []string, error) {
-	schemaProperties, err := getJsonSchemaProperties(key)
+func getSchemaPropertiesAndSampleValues(schemaName string) ([]string, []string, error) {
+	schemaProperties, err := getSchemaProperties(schemaName)
 	if err == nil {
 		properties := make([]string, 0)
 		sampleValues := make([]string, 0)
 		for key, value := range schemaProperties {
 			properties = append(properties, key)
-			sampleValues = append(sampleValues, utils.GetSampleValueByType(value.(map[string]interface{})))
+			sampleValues = append(sampleValues, utils.GetSampleValueByType(value))
 		}
 		return properties, sampleValues, nil
 	}
 	return nil, nil, err
 }
 
-func getJsonSchemaProperties(key string) (map[string]interface{}, error) {
+func getSchemaProperties(schemaName string) (spec.SchemaProperties, error) {
+	registrySwaggerSpecification := getSwaggerJson()
+	schemaDefinition, ok := registrySwaggerSpecification.Definitions[schemaName]
+	if !ok {
+		return nil, errors.New(schemaName + "schema not found")
+	}
+	return schemaDefinition.Properties, nil
+}
+
+func getSwaggerJson() spec.Swagger {
 	resp, err := http.Get(config.Config.Registry.BaseUrl + "api/docs/swagger.json")
 	utils.LogErrorIfAny("Error creating a get request for %v : %v", err, config.Config.Registry.BaseUrl+"api/docs/swagger.json")
 	body, _ := io.ReadAll(resp.Body)
-	var responseMap map[string]interface{}
+	var responseMap spec.Swagger
 	err = json.Unmarshal(body, &responseMap)
 	utils.LogErrorIfAny("Error creating request body for %v : %v", err, config.Config.Registry.BaseUrl+"api/docs/swagger.json")
-	schemaMap := responseMap["definitions"].(map[string]interface{})[key]
-	if schemaMap == nil {
-		return nil, errors.New(key + " not found")
-	}
-	schemaProperties := schemaMap.(map[string]interface{})["properties"].(map[string]interface{})
-	return schemaProperties, nil
+	return responseMap
 }
--- services/bulk_issuance/swagger_gen/restapi/operations/sample_template/get_v1_schema_name_sample_csv_responses.go
+++ services/bulk_issuance/swagger_gen/restapi/operations/sample_template/get_v1_schema_name_sample_csv_responses.go
@@ -78,6 +78,83 @@ func (o *GetV1SchemaNameSampleCsvOK) WriteResponse(rw http.ResponseWriter, produ
 	}
 }
 
+// GetV1SchemaNameSampleCsvBadRequestCode is the HTTP code returned for type GetV1SchemaNameSampleCsvBadRequest
+const GetV1SchemaNameSampleCsvBadRequestCode int = 400
+
+/*
+GetV1SchemaNameSampleCsvBadRequest Bad Request
+
+swagger:response getV1SchemaNameSampleCsvBadRequest
+*/
+type GetV1SchemaNameSampleCsvBadRequest struct {
+	/*
+
+	  Default: "application/json"
+	*/
+	ContentType string `json:"Content-Type"`
+
+	/*
+	  In: Body
+	*/
+	Payload *models.ErrorPayload `json:"body,omitempty"`
+}
+
+// NewGetV1SchemaNameSampleCsvBadRequest creates GetV1SchemaNameSampleCsvBadRequest with default headers values
+func NewGetV1SchemaNameSampleCsvBadRequest() *GetV1SchemaNameSampleCsvBadRequest {
+
+	var (
+		// initialize headers with default values
+
+		contentTypeDefault = string("application/json")
+	)
+
+	return &GetV1SchemaNameSampleCsvBadRequest{
+
+		ContentType: contentTypeDefault,
+	}
+}
+
+// WithContentType adds the contentType to the get v1 schema name sample csv bad request response
+func (o *GetV1SchemaNameSampleCsvBadRequest) WithContentType(contentType string) *GetV1SchemaNameSampleCsvBadRequest {
+	o.ContentType = contentType
+	return o
+}
+
+// SetContentType sets the contentType to the get v1 schema name sample csv bad request response
+func (o *GetV1SchemaNameSampleCsvBadRequest) SetContentType(contentType string) {
+	o.ContentType = contentType
+}
+
+// WithPayload adds the payload to the get v1 schema name sample csv bad request response
+func (o *GetV1SchemaNameSampleCsvBadRequest) WithPayload(payload *models.ErrorPayload) *GetV1SchemaNameSampleCsvBadRequest {
+	o.Payload = payload
+	return o
+}
+
+// SetPayload sets the payload to the get v1 schema name sample csv bad request response
+func (o *GetV1SchemaNameSampleCsvBadRequest) SetPayload(payload *models.ErrorPayload) {
+	o.Payload = payload
+}
+
+// WriteResponse to the client
+func (o *GetV1SchemaNameSampleCsvBadRequest) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+
+	// response header Content-Type
+
+	contentType := o.ContentType
+	if contentType != "" {
+		rw.Header().Set("Content-Type", contentType)
+	}
+
+	rw.WriteHeader(400)
+	if o.Payload != nil {
+		payload := o.Payload
+		if err := producer.Produce(rw, payload); err != nil {
+			panic(err) // let the recovery middleware deal with this
+		}
+	}
+}
+
 // GetV1SchemaNameSampleCsvNotFoundCode is the HTTP code returned for type GetV1SchemaNameSampleCsvNotFound
 const GetV1SchemaNameSampleCsvNotFoundCode int = 404
 
@@ -87,36 +164,70 @@ GetV1SchemaNameSampleCsvNotFound Not found
 swagger:response getV1SchemaNameSampleCsvNotFound
 */
 type GetV1SchemaNameSampleCsvNotFound struct {
+	/*
+
+	  Default: "application/json"
+	*/
+	ContentType string `json:"Content-Type"`
 
 	/*
 	  In: Body
 	*/
-	Payload string `json:"body,omitempty"`
+	Payload *models.ErrorPayload `json:"body,omitempty"`
 }
 
 // NewGetV1SchemaNameSampleCsvNotFound creates GetV1SchemaNameSampleCsvNotFound with default headers values
 func NewGetV1SchemaNameSampleCsvNotFound() *GetV1SchemaNameSampleCsvNotFound {
 
-	return &GetV1SchemaNameSampleCsvNotFound{}
+	var (
+		// initialize headers with default values
+
+		contentTypeDefault = string("application/json")
+	)
+
+	return &GetV1SchemaNameSampleCsvNotFound{
+
+		ContentType: contentTypeDefault,
+	}
+}
+
+// WithContentType adds the contentType to the get v1 schema name sample csv not found response
+func (o *GetV1SchemaNameSampleCsvNotFound) WithContentType(contentType string) *GetV1SchemaNameSampleCsvNotFound {
+	o.ContentType = contentType
+	return o
+}
+
+// SetContentType sets the contentType to the get v1 schema name sample csv not found response
+func (o *GetV1SchemaNameSampleCsvNotFound) SetContentType(contentType string) {
+	o.ContentType = contentType
 }
 
 // WithPayload adds the payload to the get v1 schema name sample csv not found response
-func (o *GetV1SchemaNameSampleCsvNotFound) WithPayload(payload string) *GetV1SchemaNameSampleCsvNotFound {
+func (o *GetV1SchemaNameSampleCsvNotFound) WithPayload(payload *models.ErrorPayload) *GetV1SchemaNameSampleCsvNotFound {
 	o.Payload = payload
 	return o
 }
 
 // SetPayload sets the payload to the get v1 schema name sample csv not found response
-func (o *GetV1SchemaNameSampleCsvNotFound) SetPayload(payload string) {
+func (o *GetV1SchemaNameSampleCsvNotFound) SetPayload(payload *models.ErrorPayload) {
 	o.Payload = payload
 }
 
 // WriteResponse to the client
 func (o *GetV1SchemaNameSampleCsvNotFound) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 
+	// response header Content-Type
+
+	contentType := o.ContentType
+	if contentType != "" {
+		rw.Header().Set("Content-Type", contentType)
+	}
+
 	rw.WriteHeader(404)
-	payload := o.Payload
-	if err := producer.Produce(rw, payload); err != nil {
-		panic(err) // let the recovery middleware deal with this
+	if o.Payload != nil {
+		payload := o.Payload
+		if err := producer.Produce(rw, payload); err != nil {
+			panic(err) // let the recovery middleware deal with this
+		}
 	}
 }
