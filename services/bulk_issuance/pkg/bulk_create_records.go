d22af5eb5c1176d0ff5d7b6ec9312fc6e0aaf4f3
Tejas Varade <varadetejas@gmail.com>
2023-05-23 16:57:06 +0530

    Add unit test for createRecords in bulk issuance

--- services/bulk_issuance/pkg/bulk_create_records.go
+++ services/bulk_issuance/pkg/bulk_create_records.go
@@ -4,17 +4,24 @@ import (
 	"bulk_issuance/swagger_gen/models"
 	"bulk_issuance/swagger_gen/restapi/operations/upload_and_create_records"
 	"bulk_issuance/utils"
+	"io"
+	"strings"
+
 	"github.com/Clever/csvlint"
 	"github.com/go-openapi/runtime/middleware"
 	log "github.com/sirupsen/logrus"
-	"io"
-	"strings"
 )
 
 func (c *Controllers) createRecords(params upload_and_create_records.PostV1SchemaNameUploadParams,
 	principal *models.JWTClaimBody) middleware.Responder {
 	log.Info("Creating records")
 	fileBytes, err := io.ReadAll(params.File)
+	if err != nil {
+		upload_and_create_records.NewPostV1SchemaNameUploadBadRequest().
+			WithPayload(&models.ErrorPayload{
+				Message: "Error reading request file",
+			})
+	}
 	csvError, _, _ := csvlint.Validate(strings.NewReader(string(fileBytes)), ',', false)
 	if len(csvError) != 0 {
 		return upload_and_create_records.NewPostV1SchemaNameUploadBadRequest().
