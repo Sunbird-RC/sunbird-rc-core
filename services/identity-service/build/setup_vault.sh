4ca48af4e81b810fdf2cfd8f665d3650b5aa4bfb
Tushar <codingid6@gmail.com>
2023-06-26 14:23:33 +0000

    feat: Automate vault

---
+++ services/identity-service/build/setup_vault.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+docker-compose up -d vault
+
+# Function to check if Vault is ready
+check_vault_status() {
+  vault_status=$(docker-compose exec vault vault status 2>&1)
+  if [[ $vault_status == *"Vault is sealed"* ]]; then
+    echo "Vault is sealed. Waiting for unsealing..."
+    return 1
+  elif [[ $vault_status == *"connection refused"* ]]; then
+    echo "Unable to connect to Vault. Waiting for Vault to start..."
+    return 1
+  else
+    echo "Vault is unsealed and ready to receive requests."
+    return 0
+  fi
+}
+
+
+# Wait for Vault service to become available
+until check_vault_status; do
+    echo "Waiting for Vault service to start..."
+    sleep 1;
+done
+
+docker-compose exec vault vault operator init > keys.txt
+sed -n -e 's/^Unseal Key [1-1]: \(.*\)/\1/p' keys.txt > parsed-key.txt
+key=$(cat parsed-key.txt)
+docker-compose exec -T vault vault operator unseal "$key"
+
+sed -n -e 's/^Unseal Key [2-2]: \(.*\)/\1/p' keys.txt > parsed-key.txt
+key=$(cat parsed-key.txt)
+docker-compose exec -T vault vault operator unseal "$key"
+
+sed -n -e 's/^Unseal Key [3-3]: \(.*\)/\1/p' keys.txt > parsed-key.txt
+key=$(cat parsed-key.txt)
+docker-compose exec -T vault vault operator unseal "$key"
+
+root_token=$(sed -n 's/Initial Root Token: \(.*\)/\1/p' keys.txt)
+
+sed -i "s/VAULT_TOKEN:.*/VAULT_TOKEN: $root_token/" docker-compose.yml
+
+rm keys.txt parsed-key.txt
