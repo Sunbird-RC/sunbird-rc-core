472c7ca8bc03f4cb289eb2096baa793202bb6678
Tushar <codingid6@gmail.com>
2023-06-27 09:55:06 +0000

    fix: Fix setup_vault.sh

--- services/identity-service/build/setup_vault.sh
+++ services/identity-service/build/setup_vault.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# This script does the following things
+# * Start a vault instance with vault/vault.json configuration
+# * Unseal the vault
+# * Create a v2 kv engine 
+# * Prints the unseal keys and root token
+# * This script does not automatically unseal vault on restarts, it only works with fresh installations
+
+
 docker-compose up -d vault
 
 # Function to check if Vault is ready
 check_vault_status() {
   vault_status=$(docker-compose exec vault vault status 2>&1)
-  if [[ $vault_status == *"Vault is sealed"* ]]; then
-    echo "Vault is sealed. Waiting for unsealing..."
-    return 1
-  elif [[ $vault_status == *"connection refused"* ]]; then
+  if [[ $vault_status == *"connection refused"* ]]; then
     echo "Unable to connect to Vault. Waiting for Vault to start..."
     return 1
+  elif [[ $vault_status == *"Sealed             true"* ]]; then
+    echo "Vault is sealed. Waiting for unsealing..."
+    return 0
   else
-    echo "Vault is unsealed and ready to receive requests."
+    echo "Unsealed and up. Moving to next steps."
     return 0
   fi
 }
@@ -41,4 +49,7 @@ root_token=$(sed -n 's/Initial Root Token: \(.*\)/\1/p' keys.txt)
 
 sed -i "s/VAULT_TOKEN:.*/VAULT_TOKEN: $root_token/" docker-compose.yml
 
-rm keys.txt parsed-key.txt
+docker-compose exec -e VAULT_TOKEN=$root_token -T vault vault secrets enable -path=kv kv-v2
+
+cat keys.txt
+rm parsed-key.txt
