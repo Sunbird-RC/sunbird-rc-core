f5a39b7af1a644f8bbb20f17bbdb13222acd3988
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-07-03 07:58:08 +0000

    fix: Use fixtures for test payloads

--- services/credential-schema/src/schema/schema.service.spec.ts
+++ services/credential-schema/src/schema/schema.service.spec.ts
@@ -1,59 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SchemaService } from './schema.service';
-import { CreateCredentialDTO } from './dto/create-credentials.dto';
 import { UtilsService } from '../utils/utils.service';
 import { PrismaService } from '../prisma.service';
 import { UtilsServiceMock } from '../utils/mock.util.service';
-
+import { credentialSchemaDemoPayload } from './schema.fixtures';
 describe('SchemaService', () => {
   let service: SchemaService;
-  const testCredential: CreateCredentialDTO = {
-    schema: {
-      type: 'https://w3c-ccg.github.io/vc-json-schemas/',
-      version: '1.0',
-      id: 'did:ulpschema:c9cc0f03-4f94-4f44-9bcd-b24a8696fa2',
-      name: 'Proof of Academic Evaluation Credential',
-      author: 'did:rcw:82f1ef57-3969-42db-9ef3-a477f5f7b010',
-      authored: '2022-12-19T09:22:23.064Z',
-      schema: {
-        $id: 'Proof-of-Academic-Evaluation-Credential-1.0',
-        $schema: 'https://json-schema.org/draft/2019-09/schema',
-        description:
-          'The holder has secured the <PERCENTAGE/GRADE> in <PROGRAMME> from <ABC_Institute>.',
-        type: 'object',
-        properties: {
-          grade: {
-            type: 'string',
-            description: 'Grade (%age, GPA, etc.) secured by the holder.',
-          },
-          programme: {
-            type: 'string',
-            description: 'Name of the programme pursed by the holder.',
-          },
-          certifyingInstitute: {
-            type: 'string',
-            description:
-              'Name of the instute which certified the said grade in the said skill',
-          },
-          evaluatingInstitute: {
-            type: 'string',
-            description:
-              'Name of the institute which ran the programme and evaluated the holder.',
-          },
-        },
-        required: [
-          'grade',
-          'programme',
-          'certifyingInstitute',
-          'evaluatingInstitute',
-        ],
-        additionalProperties: true,
-      },
-    },
-    tags: ['tag1', 'tag2'],
-    status: 'DRAFT',
-  };
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SchemaService, UtilsService, PrismaService],
@@ -68,7 +20,7 @@ describe('SchemaService', () => {
   });
 
   it('should create an unsigned schema and get it from DB', async () => {
-    const vcCredSchema = await service.createCredentialSchema(testCredential);
+    const vcCredSchema = await service.createCredentialSchema(credentialSchemaDemoPayload);
     expect(vcCredSchema).toBeDefined();
     expect(vcCredSchema.schema.proof).toBeFalsy();
     const getVCCredSchema = await service.getCredentialSchema({
@@ -78,7 +30,7 @@ describe('SchemaService', () => {
   });
 
   it('should create a signed schema and get it from DB', async () => {
-    const vcCredSchema = await service.createAndSignSchema(testCredential);
+    const vcCredSchema = await service.createAndSignSchema(credentialSchemaDemoPayload);
     expect(vcCredSchema).toBeDefined();
     expect(vcCredSchema.schema.proof).toBeTruthy();
     const getVCCredSchema = await service.getCredentialSchema({
