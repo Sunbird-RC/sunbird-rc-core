024c3ca8f9b080bf25c1de30d38be879cb7f6b7f
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-07-04 11:29:46 +0000

    fix: update dependencies in test

--- services/credential-schema/src/app.controller.spec.ts
+++ services/credential-schema/src/app.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaClient } from '@prisma/client';
+import { PrismaHealthIndicator } from './utils/prisma.health';
+import { TerminusModule } from '@nestjs/terminus';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,7 +11,8 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService, PrismaClient],
+      imports: [TerminusModule],
+      providers: [AppService, PrismaClient, PrismaHealthIndicator],
     }).compile();
 
     appController = app.get<AppController>(AppController);
