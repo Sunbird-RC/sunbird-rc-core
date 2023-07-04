7b46af7c7886ebba3c1714694cae57e055f0e636
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-07-04 14:08:25 +0530

    feat: Add health check for prisma

--- services/credential-schema/src/app.controller.ts
+++ services/credential-schema/src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from './utils/prisma.health';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly healthCheckService: HealthCheckService,
+    private readonly prismaIndicator: PrismaHealthIndicator,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/health')
+  @HealthCheck()
+  public async checkHealth() {
+    return this.healthCheckService.check([
+      async () => this.prismaIndicator.isHealthy('db'),
+    ]);
   }
 }
---
+++ services/credential-schema/src/utils/prisma.service.ts
@@ -0,0 +1,18 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaServiceHealthCheck
+  extends PrismaClient
+  implements OnModuleInit
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
