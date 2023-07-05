4b677c8951a5f5f1d8c3cb8eff78d91829a9c847
Tushar <codingid6@gmail.com>
2023-07-05 13:47:05 +0000

    fix: add mock generatDID

--- services/credential-schema/src/utils/mock.util.service.ts
+++ services/credential-schema/src/utils/mock.util.service.ts
@@ -15,4 +15,46 @@ export class UtilsServiceMock {
     };
     return proof;
   }
+
+  async generateDID(body: any) {
+    const mockGenerateDID = [
+      {
+        "@context": "https://w3id.org/did/v1",
+        "id": "did:rcw:98bee106-9630-4a31-ba26-177c478431cc",
+        "alsoKnownAs": [
+          "did.test@gmail.com.test"
+        ],
+        "service": [
+          {
+            "id": "IdentityHub",
+            "type": "IdentityHub",
+            "serviceEndpoint": {
+              "@context": "schema.identity.foundation/hub",
+              "@type": "UserServiceEndpoint",
+              "instance": [
+                "did:test:hub.id"
+              ]
+            }
+          }
+        ],
+        "verificationMethod": [
+          {
+            "id": "auth-key",
+            "type": "RS256",
+            "publicKeyJwk": {
+              "kty": "EC",
+              "crv": "secp256k1",
+              "x": "u6YjEMn37er9zqqS4YTnyaXOuAgJ6hRD2z9tr3Yl5HI",
+              "y": "j-BOp476lD9g_Ff0I8-wsENrDtOC4PvDQ0ssAFPTk4g"
+            },
+            "controller": "did:rcw:98bee106-9630-4a31-ba26-177c478431cc"
+          }
+        ],
+        "authentication": [
+          "auth-key"
+        ]
+      }
+    ]
+    return mockGenerateDID[0]
+  }
 }
