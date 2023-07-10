576e14de7d7f4d2f22063dbda60f61038018d0a5
Tushar <codingid6@gmail.com>
2023-07-10 05:14:52 +0000

    fix: Add identity service to healthcheck fo cred-schema

--- services/credential-schema/src/app.controller.ts
+++ services/credential-schema/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from './utils/prisma.health';
 
 @Controller()
@@ -7,12 +7,14 @@ export class AppController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
     private readonly prismaIndicator: PrismaHealthIndicator,
+    private readonly http: HttpHealthIndicator
   ) {}
   @Get('/health')
   @HealthCheck()
   public async checkHealth() {
     return this.healthCheckService.check([
       async () => this.prismaIndicator.isHealthy('db'),
+      async () => this.http.responseCheck('identity-service', `${process.env.IDENTITY_BASE_URL}/health`, (res) => res.status === 200)
     ]);
   }
 }
