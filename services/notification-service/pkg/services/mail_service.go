ce6e36a2ef6fb73d597c694708947e2fbad96a72
rkrahu <rajendra.kishor@tarento.com>
2023-08-30 14:20:26 +0530

    Send mail notification with default password while calling candidate invite for registration.

--- services/notification-service/pkg/services/mail_service.go
+++ services/notification-service/pkg/services/mail_service.go
@@ -12,14 +12,27 @@ func SendEmail(recipientEmail string, mailSubject string, mailBody string) error
 		from := config.Config.EmailSMTP.FromAddress
 		pass := config.Config.EmailSMTP.Password
 
-		msg := "From: " + from + "\n" +
-			"To: " + recipientEmail + "\n" +
-			"Subject: " + mailSubject + "\n\n" +
-			mailBody
+		// msg := "From: " + from + "\n" +
+		// 	"To: " + recipientEmail + "\n" +
+		// 	"Subject: " + mailSubject + "\n\n" +
+		// 	mailBody
 
-		err := smtp.SendMail("smtp.gmail.com:465",
-			smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-			from, []string{recipientEmail}, []byte(msg))
+		// err := smtp.SendMail("smtp.gmail.com:465",
+		// 	smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		// 	from, []string{recipientEmail}, []byte(msg))
+
+		
+		mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+		subject := "Subject: " + mailSubject+ "!\n"
+		msg := []byte(subject + mime + "\n" + mailBody)
+		addr := "smtp.gmail.com:587"	
+
+		err := smtp.SendMail(addr, smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+			from, []string{recipientEmail}, msg)
+
+
+
+		log.Infof("Message to send in mail: %s", msg)
 
 		if err != nil {
 			log.Errorf("smtp error: %s", err)
