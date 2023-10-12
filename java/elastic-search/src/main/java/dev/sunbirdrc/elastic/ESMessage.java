c1b2eb71d941233b7182d4a76cfcf3b6d16d2650
Tejaswini <tejaswini.khatavkar@tekditechnologies.com>
2023-10-12 14:32:46 +0530

    Resolved conflicts1

--- java/elastic-search/src/main/java/dev/sunbirdrc/elastic/ESMessage.java
+++ java/elastic-search/src/main/java/dev/sunbirdrc/elastic/ESMessage.java
@@ -9,9 +9,14 @@ public class ESMessage {
     String osid;
     JsonNode input;
 
-
     public ESMessage() {
     }
+
+    /*public ESMessage(String indexName, String osid, JsonNode input) {
+        setIndexName(indexName);
+        setOsid(osid);
+        setInput(input);
+    }*/
 
     public String getIndexName() {
         return indexName;
@@ -20,16 +25,20 @@ public class ESMessage {
     public void setIndexName(String indexName) {
         this.indexName = indexName;
     }
-    public String getOsid() { return osid;}
-    public void setOsid(String osid) {  this.osid = osid; }
-    public JsonNode getInput() {  return input; }
-    public void setInput(JsonNode input) {  this.input = input; }
 
-   }
+    public String getOsid() {
+        return osid;
+    }
 
+    public void setOsid(String osid) {
+        this.osid = osid;
+    }
 
+    public JsonNode getInput() {
+        return input;
+    }
 
-
-
-
-
+    public void setInput(JsonNode input) {
+        this.input = input;
+    }
+}
